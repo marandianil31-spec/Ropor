f69ddef07c149b507a466d96666051affb98c90e
root <marandianil31@gmail.com>
2026-07-13 20:04:06 +0530

    Fix Firebase package name

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
-    id("com.android.application")
+    id( "com.android.application")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
     id("com.google.gms.google-services")
 }
 
 android {
-    namespace = "com.example.random_chat_app"
+    namespace = "com.my.ropor.chat"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -17,7 +17,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.random_chat_app"
+        applicationId = "com.my.ropor.chat"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -44,3 +44,4 @@ kotlin {
 flutter {
     source = "../.."
 }
+
