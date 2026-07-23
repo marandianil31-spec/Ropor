21eab9b3a1b65c5e37f23b303d423d5e7a7c7837
marandianil31-spec <marandianil31@gmail.com>
2026-07-23 07:45:43 +0530

    Enhance joinQueue with debug prints and error handling
    Added debug prints for user status and success/error handling.

--- lib/services/match_service.dart
+++ lib/services/match_service.dart
@@ -2,21 +2,28 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 
 class MatchService {
-  static final DatabaseReference db =
-      FirebaseDatabase.instance.ref();
+  static final DatabaseReference db = FirebaseDatabase.instance.ref();
 
   static Future<void> joinQueue() async {
     final user = FirebaseAuth.instance.currentUser;
-    if (user == null) return;
+
+    print("USER: $user");
+
+    if (user == null) {
+      print("User is NULL");
+      return;
+    }
 
     try {
-  await db.child("waiting").child(user.uid).set({
-    "uid": user.uid,
-    "time": ServerValue.timestamp,
-  });
-  print("SUCCESS");
-} catch (e) {
-  print(e);
+      await db.child("waiting").child(user.uid).set({
+        "uid": user.uid,
+        "time": ServerValue.timestamp,
+      });
+
+      print("Waiting node created successfully");
+    } catch (e, s) {
+      print("ERROR: $e");
+      print(s);
+    }
   }
- }
 }
