3f6e8c2083a6d21e4a6321b703a9a30e8a02ed52
marandianil31-spec <marandianil31@gmail.com>
2026-07-23 06:56:26 +0530

    Add AppColors class for theme colors

---
+++ lib/lib/theme/app_colors.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Background
+  static const Color background = Color(0xFF080808);
+
+  // Primary Purple
+  static const Color primary = Color(0xFF7C3AED);
+
+  // Card
+  static const Color card = Color(0xFF1A1A1A);
+
+  // Text
+  static const Color white = Colors.white;
+  static const Color grey = Color(0xFF9E9E9E);
+
+  // Button
+  static const Color button = Color(0xFF7C3AED);
+
+  // Success
+  static const Color success = Color(0xFF22C55E);
+
+  // Error
+  static const Color error = Color(0xFFEF4444);
+}
