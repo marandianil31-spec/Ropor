f68f2850a17d8153cc9bde74eb0b9d9512009fec
marandianil31-spec <marandianil31@gmail.com>
2026-07-26 03:39:27 +0530

    Remove test code
    Removed unused imports and the HomeScreen widget implementation.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_database/firebase_database.dart';
 
 import 'firebase_options.dart';
-import 'services/auth_service.dart';
+import 'screens/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -24,50 +22,6 @@ class RoporChat extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: const HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Ropor Chat"),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          child: const Text("Test Firebase"),
-          onPressed: () async {
-            try {
-              final user = await AuthService.signInAnonymously();
-
-              await FirebaseDatabase.instance.ref("test").set({
-                "uid": user!.uid,
-                "message": "Hello",
-              });
-
-              if (context.mounted) {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(
-                    content: Text("Database Write Success"),
-                  ),
-                );
-              }
-            } catch (e) {
-              if (context.mounted) {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text(e.toString()),
-                  ),
-                );
-              }
-            }
-          },
-        ),
-      ),
     );
   }
 }
