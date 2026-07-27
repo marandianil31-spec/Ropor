fba2b4a71d65976cc84e0b02c9d6a91c63df23e0
marandianil31-spec <marandianil31@gmail.com>
2026-07-27 08:16:08 +0530

    Add app_theme.dart

---
+++ lib/lib/theme/app_theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color primary = Color(0xFF6A1B9A);
+  static const Color accent = Color(0xFF8E24AA);
+  static const Color background = Color(0xFFF8F8F8);
+  static const Color online = Color(0xFF4CAF50);
+  static const Color report = Color(0xFFE53935);
+  static const Color white = Colors.white;
+  static const Color black = Colors.black87;
+}
