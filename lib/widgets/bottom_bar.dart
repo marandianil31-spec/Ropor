dbdc61fb07e561b4b73f23c659cd66afae3b4f8d
marandianil31-spec <marandianil31@gmail.com>
2026-07-28 07:04:49 +0530

    Update BottomBar UI

--- lib/widgets/bottom_bar.dart
+++ lib/widgets/bottom_bar.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 
-class BottomBar extends StatelessWidget {
+class BottomBar extends StatefulWidget {
   const BottomBar({super.key});
 
   @override
+  State<BottomBar> createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  final TextEditingController controller = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
+    final bool hasText = controller.text.trim().isNotEmpty;
+
     return SafeArea(
       child: Container(
         padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
@@ -12,10 +21,17 @@ class BottomBar extends StatelessWidget {
           children: [
             IconButton(
               icon: const Icon(Icons.attach_file),
-              onPressed: () {},
+              onPressed: () {
+                // TODO: Attachment Menu
+              },
             ),
+
             Expanded(
               child: TextField(
+                controller: controller,
+                onChanged: (_) {
+                  setState(() {});
+                },
                 decoration: InputDecoration(
                   hintText: "Type a message...",
                   border: OutlineInputBorder(
@@ -24,9 +40,25 @@ class BottomBar extends StatelessWidget {
                 ),
               ),
             ),
-            IconButton(
-              icon: const Icon(Icons.send),
-              onPressed: () {},
+
+            const SizedBox(width: 8),
+
+            CircleAvatar(
+              radius: 24,
+              backgroundColor: Colors.deepPurple,
+              child: IconButton(
+                icon: Icon(
+                  hasText ? Icons.send : Icons.sync,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  if (hasText) {
+                    // TODO: Send Message
+                  } else {
+                    // TODO: Disconnect / Next
+                  }
+                },
+              ),
             ),
           ],
         ),
