c49484d5b355988d7e293019fbd1e543fa5e568f
marandianil31-spec <marandianil31@gmail.com>
2026-07-28 09:00:21 +0530

    Implement ChatService for sending messages

---
+++ lib/services/chat_service.dart
@@ -0,0 +1,21 @@
+import 'package:firebase_database/firebase_database.dart';
+
+class ChatService {
+  final DatabaseReference _db = FirebaseDatabase.instance.ref();
+
+  DatabaseReference messagesRef(String roomId) {
+    return _db.child("chatrooms").child(roomId).child("messages");
+  }
+
+  Future<void> sendMessage({
+    required String roomId,
+    required String senderId,
+    required String text,
+  }) async {
+    await messagesRef(roomId).push().set({
+      "senderId": senderId,
+      "text": text,
+      "timestamp": ServerValue.timestamp,
+    });
+  }
+}
