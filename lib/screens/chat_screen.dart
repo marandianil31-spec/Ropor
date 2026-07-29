22e577a31a077797ea71488ce15666197813b139
marandianil31-spec <marandianil31@gmail.com>
2026-07-29 11:55:47 +0530

    Fix formatting of Expanded widget in chat_screen.dart

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -31,7 +31,7 @@ class _ChatScreenState extends State<ChatScreen> {
     children: [
       const WarningCard(),
 
-      const Expanded(
+       Expanded(
         child: SingleChildScrollView(
           child: Column(
             children: [
