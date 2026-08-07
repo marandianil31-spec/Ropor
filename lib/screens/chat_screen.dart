f99f36881096697fd6157f1cba431a6f7bf50dcf
marandianil31-spec <marandianil31@gmail.com>
2026-08-07 15:43:10 +0530

    Add block_service import to chat_screen.dart

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,4 +1,5 @@
 import '../services/report_service.dart';
+import '../services/block_service.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
